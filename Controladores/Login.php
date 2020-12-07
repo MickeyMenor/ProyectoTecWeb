8352749d8f41e08f2132c00cb892f2e3134cdaa0
MickeyMenor <miguel.menor09@gmail.com>
2020-12-07 16:40:10 -0600

    Creación de formularios
    Se crearon y empezaron a trabajar algunos formularios...

--- Controladores/Login.php
+++ Controladores/Login.php
@@ -5,4 +5,5 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+header("Location: http://localhost:8080/ProyectoTecWeb/Vistas/Login.php");
+echo '<h1> hola equis de </h1>';
